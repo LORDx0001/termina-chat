0568209c9559d0c374911bbd35df1dda565d1467
LORDx0001 <bexruztoj2005gmail.com>
2025-10-03 16:00:57 +0500

    Terminal chat v8

--- auto_launcher.py
+++ auto_launcher.py
@@ -2,13 +2,16 @@ import subprocess
 import socket
 import time
 import os
+import logging
+
+logging.basicConfig(filename="/root/terminal-chat/launcher.log", level=logging.INFO)
 
 def is_port_open(port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         return s.connect_ex(('0.0.0.0', port)) == 0
 
 def launch_server(port):
-    print(f"[+] Запускаю сервер на порту {port}...")
+    logging.info(f"Запускаю сервер на порту {port}")
     subprocess.Popen(['python3', 'server.py', str(port)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 def monitor_ports():
